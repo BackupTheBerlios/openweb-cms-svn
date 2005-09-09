73f7d3fed05f6e0f8758884a6d0b0613cb716427
laurentj <laurentj@95276fae-7dd8-0310-bfa4-98f3a3248224>
2005-09-09 21:25:45 +0000

    FIX 5007 : erreur sablotron quand il y avait des erreurs php

--- branches/v1/include/frontend/initmini.inc.php
+++ branches/v1/include/frontend/initmini.inc.php
@@ -16,9 +16,37 @@ header("Content-type: text/html; charset=utf-8");
 @define('PATH_INC_FRONTEND', dirname(__FILE__).'/');
 @define('PATH_INC_BASECLASS', PATH_INCLUDE.'baseclass/');
 
-error_reporting(E_ALL);
-
 setlocale(LC_TIME, 'fr_FR');
 setlocale(LC_MESSAGES, 'fr_FR');
 
+
+function fctErrorHandler($errno, $errmsg, $filename, $linenum, $errcontext){
+
+    $codeString = array(
+        E_ERROR         => 'ERROR',
+        E_WARNING       => 'WARNING',
+        E_NOTICE        => 'NOTICE',
+        E_USER_ERROR    => 'USER_ERROR',
+        E_USER_WARNING  => 'USER_WARNING',
+        E_USER_NOTICE   => 'USER_NOTICE',
+        E_STRICT        => 'STRICT'
+        );
+
+    if (error_reporting() == 0)
+        return;
+
+    $msg = date("Y-m-d H:i:s") .' ['.$codeString[$errno].'] file:'.$filename.' line:'
+         .$linenum."\n\t".$errmsg."\n";
+
+    error_log($msg,3, PATH_SITE_ROOT.'temp/error.log');
+
+
+    if($errno == E_ERROR || $errno == E_USER_ERROR ){
+        echo 'Erreur fatale sur la page.';
+        exit;
+    }
+}
+
+set_error_handler('fctErrorHandler');
+// error_reporting(E_ALL); seulement en dev, et seulement si tu désactive le handler fctErrorHandler !
 ?>
